79a58a675d231f9c80954ae0b72d880a31758a07
sayan25saha <82870713+sayan25saha@users.noreply.github.com>
2021-07-12 12:35:49 +0530

    Add files via upload

---
+++ SnakeMatrix.java
@@ -0,0 +1,36 @@
+package Coding;
+
+public class SnakeMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+                {1, 2, 3},
+               };
+
+        // In case of an array of size n*n put n in place i & j in the for loop
+
+
+        for (int i = 0; i < 4; i++) {
+
+            if ((i+1)%2 == 1) {
+
+                for (int j = 0; j < 3; j++) {
+                    System.out.print(arr[i][j]);
+
+                }
+
+            } else {
+
+                for (int k = 2; k>=0; k--) {
+                    System.out.print(arr[i][k]);
+
+
+                }
+            }
+System.out.print(" ");
+
+        }
+    }
+}
+
